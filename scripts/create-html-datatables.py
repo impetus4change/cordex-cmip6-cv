61236cf86db150fa51375a989bce5c3c05673662
Jesus Fernandez <jesusff@users.noreply.github.com>
2024-04-16 13:41:21 +0200

    Replace CORDEX -> I4C in html datatables

--- scripts/create-html-datatables.py
+++ scripts/create-html-datatables.py
@@ -120,8 +120,8 @@ span.running {color: #009900}
 span.completed {color: black; font-weight: bold}
 span.published {color: #3399FF; font-weight: bold}
 span.warning {color: #FF0000; font-weight: bold}
-a:link { text-decoration: none; }
-a:visited { text-decoration: none; }
+a:link { color: #3399FF; text-decoration: none; }
+a:visited { color: #3399FF; text-decoration: none; }
 a:hover { text-decoration: underline; }
 a:active { text-decoration: underline; }
 </style>
@@ -170,7 +170,7 @@ link_header = (
     + " · ".join([f'<a href="{table_prefix}_{x}.html">{x} table</a>' for x in cvs])
     + "<p>"
 )
-cordex_cv_repo = '<a href="https://github.com/WCRP-CORDEX/cordex-cmip6-cv">CORDEX-CMIP6 CV repository</a>'
+cordex_cv_repo = '<a href="https://github.com/impetus4change/cordex-cmip6-cv">I4C CV repository</a>'
 display_options = {
     "source_id": {
         "columns": [
@@ -185,14 +185,14 @@ display_options = {
             "license",
         ],
         "is_1d": False,
-        "intro": f'{link_header}Registered models. Visit the {cordex_cv_repo} to register or update your model. <span class="warning">This is a test page.</span>',
+        "intro": f'{link_header}List of registered models. Visit the {cordex_cv_repo} to register or update your model.',
         "column_as_link": "source_id",
         "column_as_link_source": "further_info_url",
     },
     "institution_id": {
         "columns": ["institution_id", "institution"],
         "is_1d": True,
-        "intro": f'{link_header}Registered institutions. Visit the {cordex_cv_repo} to register or update your institution details. <span class="warning">This is a test page.</span>',
+        "intro": f'{link_header}List of registered institutions. Visit the {cordex_cv_repo} to register or update your institution details.',
     },
 }
 
@@ -203,6 +203,6 @@ if __name__ == "__main__":
             f"{table_prefix}_{cv}.json",
             f"docs/{table_prefix}_{cv}.html",
             cv,
-            title=f"WCRP-CORDEX CORDEX-CMIP6 CV {cv}",
+            title=f"Impetus4Change downscaling (WP3) CV {cv}",
             **opts,
         )
